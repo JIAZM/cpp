f6209557dd1db01e84048fdda41aba3bd41989e8
jiazm <18846936491@163.com>
2020-02-09 13:08:51 +0800

    更改目录结构，增加编译脚本文件

---
+++ sources/Compiler.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+name=`echo $1 | awk -F "." '{print $1}'`
+attr=`echo $1 | awk -F "." '{print $2}'`
+
+#echo name = $name
+#echo attr = $attr
+
+if [ $attr = "cpp" ] ; then
+	echo attr is .cpp
+	echo Choose compiler g++
+	CC=g++
+elif [ $attr = "c" ] ; then
+	echo attr is .c
+	echo Choose compiler gcc
+	CC=gcc
+fi
+
+$CC $name.$attr -o released/$name
+
+if [ $? -eq 0 ] ; then
+	echo Compile success;
+	echo Compiled file moved to floder \[compiled\]
+else
+	echo Compile fault;
+fi
